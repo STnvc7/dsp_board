e0c9d4e068e634d2c7ca81c63534d9d7d238af51
k_hiro <pk336z7e@s.okayama-u.ac.jp>
2025-11-11 16:48:17 +0900

    modify for setuptools

--- dsp_board/__init__.py
+++ dsp_board/__init__.py
@@ -1,7 +1,6 @@
-from . import features, preprocesses, transforms
-from .processor import Processor
-from .utils.parallelize import set_max_workers, set_enable_parallel, get_max_workers, get_enable_parallel
-
+from dsp_board import features, preprocesses, transforms
+from dsp_board.processor import Processor
+from dsp_board.utils.parallelize import set_max_workers, set_enable_parallel, get_max_workers, get_enable_parallel
 __all__ = [
     "Processor",
     "features",
